87ec5f5c5b5dd78243b7e78ed0499b06132d0b44
Michael K. Hunter <hunter.michael.k@gmail.com>
2025-12-11 14:29:23 -0500

    First unit test and view function.

--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
 # Create your views here.
+def home_page(request):
+  return HttpResponse("<html><head><title>To-Do lists</title></head><body></body></html>")
