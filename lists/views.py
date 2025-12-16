86aaed0200f20ab522b306b51770861142631814
Michael K. Hunter <hunter.michael.k@gmail.com>
2025-12-15 22:29:20 -0500

    Displaying one list at a time.

--- lists/views.py
+++ lists/views.py
@@ -9,11 +9,10 @@ def home_page(request):
   )
 def view_list(request, list_id):
   our_list = List.objects.get(id=list_id)
-  items = Item.objects.filter(list=our_list)
   return render (
     request,
     'list.html',
-    {"items": items}
+    {"list": our_list}
   )
 
 def new_list(request):
