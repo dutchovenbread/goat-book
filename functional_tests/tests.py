8145734163cccdc506b9f420c7e7a2de9455cb03
Michael K. Hunter <hunter.michael.k@gmail.com>
2025-12-16 16:31:44 -0500

    Added more margin on the test, but now the functional test successfully checks the formatting.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,6 +1,6 @@
 import unittest
 import time
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import WebDriverException
@@ -8,7 +8,7 @@ from selenium.webdriver.common.keys import Keys
 
 MAX_WAIT = 5
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
   def setUp(self):
     self.browser = webdriver.Firefox()
 
@@ -126,7 +126,7 @@ class NewVisitorTest(LiveServerTestCase):
     self.assertAlmostEqual(
       inputbox.location['x'] + inputbox.size['width'] / 2,
       512,
-      delta=10
+      delta=30
     )
 
     # She starts a new list and sees the input is
@@ -138,5 +138,5 @@ class NewVisitorTest(LiveServerTestCase):
     self.assertAlmostEqual(
       inputbox.location['x'] + inputbox.size['width'] / 2,
       512,
-      delta=10
+      delta=30
     )
