99ef2310e2b9c2bb8e131b931f192aff7bfcec8a
Michael K. Hunter <hunter.michael.k@gmail.com>
2025-12-29 18:15:34 -0500

    Added new helper to 'base' and completed the FT for blank validation.

--- src/functional_tests/test_list_item_validation.py
+++ src/functional_tests/test_list_item_validation.py
@@ -1,4 +1,3 @@
-from unittest import skip
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
@@ -7,25 +6,43 @@ from .base import FunctionalTest
 
 
 class ItemValidationTest(FunctionalTest):
-  @skip
+
   def test_cannot_add_empty_list_items(self):
     # Edith goes to the home page and tries to submit
     # an empty list item. She hits Enter on the empty input box
-    # self.browser.get(self.live_server_url)
-    # inputbox = self.browser.find_element(By.ID, 'id_new_item')
-    # inputbox.send_keys(Keys.ENTER)
+    self.browser.get(self.live_server_url)
+    inputbox = self.browser.find_element(By.ID, 'id_new_item')
+    inputbox.send_keys(Keys.ENTER)
 
     # The home page refreshes, and there is an error message
     # saying that list items cannot be blank
     # self.wait_for_row_in_list_table("You can't have an empty list item")
+    self.wait_for(
+      lambda: self.assertEqual(
+        self.browser.find_element(By.CSS_SELECTOR, '.invalid-feedback').text,
+        "You can't have an empty list item"
+      )
+    )
 
     # She tries again with some text for the item, which now works
-    # inputbox = self.browser.find_element(By.ID, 'id_new_item')
-    # inputbox.send_keys('Buy milk')
-    # inputbox.send_keys(Keys.ENTER)
-    # self.wait_for_row_in_list_table('1: Buy milk')
+    inputbox = self.browser.find_element(By.ID, 'id_new_item')
+    inputbox.send_keys('Purchase milk')
+    inputbox.send_keys(Keys.ENTER)
+    self.wait_for_row_in_list_table('1: Purchase milk')
 
     # Perversely, she now decides to submit a second blank list item
+    inputbox = self.browser.find_element(By.ID, 'id_new_item')
+    inputbox.send_keys(Keys.ENTER)
+
+    # She receives a similar warning on the list page
+    self.wait_for(
+      lambda: self.assertEqual(
+        self.browser.find_element(By.CSS_SELECTOR, '.invalid-feedback').text,
+        "You can't have an empty list item"
+      )
+    )
 
     # And she can correct it by filling some text in
-    self.fail('Write me!')
+    self.browser.find_element(By.ID, 'id_new_item').send_keys('Make tea')
+    self.browser.find_element(By.ID, 'id_new_item').send_keys(Keys.ENTER)
+    self.wait_for_row_in_list_table('2: Make tea')
