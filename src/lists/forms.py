8295d3a3040cbdd3aba1655a734ca39d4c552f7e
Michael K. Hunter <hunter.michael.k@gmail.com>
2026-01-03 21:39:42 -0500

    Tested validation in form.

--- src/lists/forms.py
+++ src/lists/forms.py
@@ -2,6 +2,8 @@ from django import forms
 
 from lists.models import Item
 
+EMPTY_ITEM_ERROR = "You can't have an empty list item"
+
 class ItemForm(forms.models.ModelForm):
   class Meta:
     model = Item
@@ -14,11 +16,6 @@ class ItemForm(forms.models.ModelForm):
         }
       )
     }
-  # item_text = forms.CharField(
-  #   widget=forms.TextInput (
-  #     attrs={
-  #       'placeholder': 'Enter a to-do item',
-  #       'class': 'form-control input-lg'
-  #     }
-  #   )
-  # )
+    error_messages = {
+      'text': {'required': EMPTY_ITEM_ERROR}
+    }
