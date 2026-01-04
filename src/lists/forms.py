e58d1119814ded3380eae6b1523b1d8ae76af182
Michael K. Hunter <hunter.michael.k@gmail.com>
2026-01-03 21:24:25 -0500

    Convert to ModelForm

--- src/lists/forms.py
+++ src/lists/forms.py
@@ -1,11 +1,24 @@
 from django import forms
 
-class ItemForm(forms.Form):
-  item_text = forms.CharField(
-    widget=forms.TextInput (
-      attrs={
-        'placeholder': 'Enter a to-do item',
-        'class': 'form-control input-lg'
-      }
-    )
-  )
+from lists.models import Item
+
+class ItemForm(forms.models.ModelForm):
+  class Meta:
+    model = Item
+    fields = ['text',]
+    widgets = {
+      'text': forms.TextInput(
+        attrs={
+          'placeholder': 'Enter a to-do item',
+          'class': 'form-control input-lg'
+        }
+      )
+    }
+  # item_text = forms.CharField(
+  #   widget=forms.TextInput (
+  #     attrs={
+  #       'placeholder': 'Enter a to-do item',
+  #       'class': 'form-control input-lg'
+  #     }
+  #   )
+  # )
