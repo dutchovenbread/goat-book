b604381fa1d9682fbf10143692e1ef477cb44397
Michael K. Hunter <hunter.michael.k@gmail.com>
2026-01-11 15:22:36 -0500

    Added constraint to model and reworked tests.

--- src/lists/tests/test_models.py
+++ src/lists/tests/test_models.py
@@ -7,32 +7,17 @@ from lists.views import home_page
 import lxml.html
 
 class ListAndItemModelTest(TestCase):
-  def test_saving_and_retrieving_items(self):
-    mylist = List()
-    mylist.save()
+  def test_default_text(self):
+    item = Item()
+    self.assertEqual(item.text, '')
 
-    first_item = Item()
-    first_item.text = 'The first (ever) list item'
-    first_item.list = mylist
-    first_item.save()
+  def test_item_is_related_to_list(self):
+    mylist = List.objects.create()
+    item = Item()
+    item.list = mylist
+    item.save()
+    self.assertIn(item, mylist.item_set.all())
 
-    second_item = Item()
-    second_item.text = 'Item the second'
-    second_item.list = mylist
-    second_item.save()
-
-    saved_list = List.objects.get()
-    self.assertEqual(saved_list, mylist)
-
-    saved_items = Item.objects.all()
-    self.assertEqual(saved_items.count(), 2)
-
-    first_saved_item = saved_items[0]
-    second_saved_item = saved_items[1]
-    self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-    self.assertEqual(first_saved_item.list, mylist)
-    self.assertEqual(second_saved_item.text, 'Item the second')
-    self.assertEqual(second_saved_item.list, mylist)
   def test_cannot_save_empty_list_items(self):
     mylist = List.objects.create()
     item = Item(list=mylist, text='')
@@ -49,3 +34,18 @@ class ListAndItemModelTest(TestCase):
   def test_get_absolute_url(self):
     mylist = List.objects.create()
     self.assertEqual(mylist.get_absolute_url(), f'/lists/{mylist.id}/')
+
+  def test_duplicate_items_are_invalid(self):
+    mylist = List.objects.create()
+    Item.objects.create(list=mylist, text='duplicate')
+    with self.assertRaises(ValidationError):
+      item = Item(list=mylist, text='duplicate')
+      item.full_clean()
+
+  def test_CAN_save_same_item_to_different_lists(self):
+    list1 = List.objects.create()
+    list2 = List.objects.create()
+    Item.objects.create(list=list1, text='duplicate')
+    item = Item(list=list2, text='duplicate')
+    # This should not raise
+    item.full_clean()
