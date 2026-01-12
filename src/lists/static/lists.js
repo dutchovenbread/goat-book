6d0f335a7701a330b36ce60bc6a65312319891e8
Michael K. Hunter <hunter.michael.k@gmail.com>
2026-01-12 14:56:26 -0500

    Refactor to remove unused selector.

--- src/lists/static/lists.js
+++ src/lists/static/lists.js
@@ -1,9 +1,6 @@
-console.log("lists.js loading");
-const initialize = (inputSelector, errorSelector) => {
-  console.log("initialie called");
+const initialize = (inputSelector) => {
   const textInput = document.querySelector(inputSelector);
   textInput.oninput = () => {
-    const errorMsg = document.querySelector(errorSelector);
-    errorMsg.style.display = "none";
+    textInput.classList.remove("is-invalid");
   };
 };
