be027ee756e5ce3de4ebd6a8a5f1cb16c046578b
Michael K. Hunter <hunter.michael.k@gmail.com>
2026-01-11 21:56:53 -0500

    Finish out the prevention of redundant list items.

--- src/lists/models.py
+++ src/lists/models.py
@@ -7,8 +7,14 @@ class Item(models.Model):
   list = models.ForeignKey('List', default=None, on_delete=models.CASCADE)
 
   class Meta:
+    ordering = ['id']
     unique_together = ('list', 'text')
+
+  def __str__(self):
+    return self.text
 
 class List(models.Model):
   def get_absolute_url(self):
     return reverse("view_list", args=[self.id])
+  
+
