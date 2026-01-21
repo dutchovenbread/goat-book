7f194a5d1b4b3408fe3f7cd67c29c442b93d77b5
Michael K. Hunter <hunter.michael.k@gmail.com>
2026-01-21 14:39:47 -0500

    The code I tried.

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,33 +1,17 @@
+import uuid
+
 from django.db import models
-from django.contrib.auth.models import(
-  AbstractBaseUser,
-  BaseUserManager
-)
+
+
+class User(models.Model):
+    email = models.EmailField(primary_key=True)
+
+    REQUIRED_FIELDS = []
+    USERNAME_FIELD = "email"
+    is_anonymous = False
+    is_authenticated = True
+
 
 class Token(models.Model):
-  email = models.EmailField()
-  uid = models.CharField(max_length=255)
-
-class ListUserManager(BaseUserManager):
-    def create_user(self,email):
-      ListUser.objects.create(email=email)
-
-    def create_superuser(self,email,password):
-      self.create_user(email)
-
-
-class ListUser(AbstractBaseUser):
-  _email = models.EmailField(primary_key=True)
-  USERNAME_FIELD = _email
-  # REQUIRED_FIELDS = ['email', 'height']
-
-  objects = ListUserManager()
-
-  @property
-  def is_staff(self):
-    return self._email == "harry.percival@example.com"
-  
-  @property
-  def is_active(self):
-    return True
-
+    email = models.EmailField()
+    uid = models.CharField(default=uuid.uuid4, max_length=40)
