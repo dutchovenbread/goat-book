a4ca700df69e94c58f622d49c3aaa790e1c2eeee
Michael K. Hunter <hunter.michael.k@gmail.com>
2026-01-22 14:10:20 -0500

    Using monkey patching.

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.core.mail import send_mail
+from django.shortcuts import redirect
 
-# Create your views here.
+def send_login_email(request):
+  email = request.POST["email"]
+  # expected future code:
+  send_mail(
+    "Your login link for Superlists",
+    "Some kind of body",
+    "noreply@superlists.com",
+    [email],
+  )
+  return redirect("/")
