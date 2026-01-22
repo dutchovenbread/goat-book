2dad72fb0b3670ee8b11c0184f92bfbef103738d
Michael K. Hunter <hunter.michael.k@gmail.com>
2026-01-22 16:04:36 -0500

    Intermediate step. Added url for login.

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.core.mail import send_mail
 from django.shortcuts import redirect
 
@@ -6,8 +7,15 @@ def send_login_email(request):
   # expected future code:
   send_mail(
     "Your login link for Superlists",
-    "Some kind of body",
+    "Use this link to log in",
     "noreply@superlists.com",
     [email],
   )
+  messages.success(
+    request,
+    "Check your email, we've sent you a link you can use to log in."
+  )
+  return redirect("/")
+
+def login(request):
   return redirect("/")
