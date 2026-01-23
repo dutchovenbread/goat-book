02d743159679385028104f16301061d78050cd15
Michael K. Hunter <hunter.michael.k@gmail.com>
2026-01-23 12:27:19 -0500

    Correct a small inconsistency between my code and the book.

--- src/superlists/settings.py
+++ src/superlists/settings.py
@@ -160,6 +160,6 @@ LOGGING = {
 
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER = "hunter.michael.k@gmail.com"
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
