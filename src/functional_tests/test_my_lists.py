aff3034ea4da0706095ae5c1a6402284415a26b9
Michael K. Hunter <hunter.michael.k@gmail.com>
2026-01-26 08:23:07 -0500

    Added early return for the refactoring effort.

--- src/functional_tests/test_my_lists.py
+++ src/functional_tests/test_my_lists.py
@@ -47,6 +47,8 @@ class MyListsTest(FunctionalTest):
         self.browser.find_element(By.CSS_SELECTOR, "h1").text)
     )
 
+    return # TODO: resume here after templates refactor
+
     # And she sees that her list is in there,
     # named according to its first item
     self.wait_for(
